3f49a504009e9542d8e52551728ebd9462f5632f
Vini-Araujoo <241025425@aluno.unb.br>
2025-03-21 13:11:38 -0300

    Parte esquerda da página de cadastro concluída

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -5,16 +5,19 @@ const Cadastro = () => {
         <div className={styles.cadastro}>
             <div className={styles.container}>
                 <div className={styles.caixaEsquerda}>
-                    <h1>Crie sua <br /><span>Conta</span></h1>
-                        <form className={styles.formulario} action="">
-                            <label className={styles.labels} htmlFor="nome">Nome</label>
-                            <input className={styles.inputs} type="text" name="nome" id="nome" placeholder="Digite seu nome"/>
-                            <label className={styles.labels} htmlFor="email">E-mail</label>
-                            <input className={styles.inputs} type="email" name="email" id="email" placeholder="Digite seu e-mail "/>
-                            <label className={styles.labels} htmlFor="senha">Senha</label>
-                            <input className={styles.inputs} type="password" name="senha" id="senha" placeholder="Digite sua senha "/>
-
-                        </form>
+                    <div className={styles.titulo}>
+                        <h1>Crie sua <br /><span className={styles.spanVerde}>Conta</span></h1>
+                    </div>
+                    <div className={styles.linha}></div>
+                    <form className={styles.formulario} action="">
+                        <label className={styles.labels} htmlFor="nome">Nome</label>
+                        <input className={styles.inputs} type="text" name="nome" id="nome" placeholder="Digite seu nome"/>
+                        <label className={styles.labels} htmlFor="email">E-mail</label>
+                        <input className={styles.inputs} type="email" name="email" id="email" placeholder="Digite seu e-mail "/>
+                        <label className={styles.labels} htmlFor="senha">Senha</label>
+                        <input className={styles.inputs} type="password" name="senha" id="senha" placeholder="Digite sua senha "/>
+                        <button className={styles.botao} type="submit">Cadastre-se</button>
+                    </form>
                 </div>
 
                 <div className={styles.caixaDireita}>
