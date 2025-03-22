ed82831ec5b375296f8163dac5c2f3975b518408
Acciolyy <thiagoacciolyest@gmail.com>
2025-03-21 23:21:55 -0300

    Linkando icones e responsividade

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "",
+        path: "/",
         element: <HomePage />,
       },
       
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -23,9 +23,9 @@ const HomePage = () => {
       </div>
       <nav className={styles.sideNav}>
         <ul>
-          <li><a href="#" className={styles.navItem}><i className={styles.homeIcon}></i></a></li>
-          <li><a href="#" className={styles.navItem}><i className={styles.profileIcon}></i></a></li>
-          <li><a href="#" className={styles.navItem}><i className={styles.menuIcon}></i></a></li>
+          <li><a href="/" className={styles.navItem}><i className={styles.homeIcon}></i></a></li>
+          <li><a href="/cadastro" className={styles.navItem}><i className={styles.profileIcon}></i></a></li>
+          <li><a href="/menu" className={styles.navItem}><i className={styles.menuIcon}></i></a></li>
         </ul>
       </nav>
     </div>
