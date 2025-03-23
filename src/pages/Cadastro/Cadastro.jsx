88aca2ba485db42cda90995c17645f88974bf339
Vini-Araujoo <241025425@aluno.unb.br>
2025-03-23 18:02:37 -0300

    Imagens adicionadas

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,10 +1,17 @@
 import styles from './Cadastro.module.css';
+import mascote from '../../assets/orc.png'; 
+import decoracaoEsquerda from '../../assets/detalheVerdeClaro.svg';
+import decoracaoDireita from '../../assets/decoracaoVerdeDireita.svg';
 
 const Cadastro = () => {
     return (
         <div className={styles.cadastro}>
+
             <div className={styles.container}>
+                <img src={mascote} alt="Mascote" className={styles.mascote} />
+                <img src={decoracaoDireita} alt="Decoração do lado direito" className={styles.decoracaoDireita} />
                 <div className={styles.caixaEsquerda}>
+                    <img src={decoracaoEsquerda} alt="Decoração do lado esquerdo" className={styles.decoracaoEsquerda} />
                     <div className={styles.titulo}>
                         <h1>Crie sua <br /><span className={styles.spanVerde}>Conta</span></h1>
                     </div>
