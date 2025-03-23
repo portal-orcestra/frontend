a4bfd3622b9b7f137678462fe592b6172beed8c7
Acciolyy <thiagoacciolyest@gmail.com>
2025-03-23 17:50:42 -0300

    Logo e Flexbox

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,26 +1,9 @@
 import styles from './HomePage.module.css';
-import orcImage from '../../assets/orc2.png'; 
+import orcImage from '../../assets/Logo.svg'; 
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            <span className={styles.titleDark}>Bem-</span>
-            <span className={styles.titleGreen}>vindo ao</span>
-          </h1>
-          <h2 className={styles.subtitle}>
-            <span className={styles.subtitleGreen}>Portal</span>
-            <span className={styles.subtitleDark}>Orc</span>
-          </h2>
-        </div>
-        <div className={styles.imageContainer}>
-          <div className={styles.orcWrapper}>
-            <img src={orcImage} alt="Orc mascote" className={styles.orcImage} />
-          </div>
-        </div>
-      </div>
       <nav className={styles.sideNav}>
         <ul>
           <li><a href="/" className={styles.navItem}><i className={styles.homeIcon}></i></a></li>
@@ -28,6 +11,9 @@ const HomePage = () => {
           <li><a href="/menu" className={styles.navItem}><i className={styles.menuIcon}></i></a></li>
         </ul>
       </nav>
+      <div className={styles.content}>
+        <img src={orcImage} alt="Orc mascote" className={styles.orcImage} />
+      </div>
     </div>
   );
 };
